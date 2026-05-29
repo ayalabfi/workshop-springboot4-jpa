d4901382ec52fd079e3c9d032b8e39e7fb14aafa
Ayala Borba Filho <ayalaborbafilho@gmail.com>
2026-05-29 13:04:37 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.resources.exceptions;
 
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -34,6 +35,31 @@ public class ResourceExceptionHandler {
                 // URI da requisição
                 request.getRequestURI());
         // Constrói a resposta HTTP com status 404 e o Body retorna o objeto StandardError que é convertido para JSON automáticamente pelo Jackson
+        return ResponseEntity.status(status).body(err);
+    }
+
+    // Indica que esse método será executado sempre que essa exceção for lançada em qualquer controller
+    @ExceptionHandler(DatabaseException.class)
+    // ResourceNotFoundException e: A própria exceção é capturada, permitindo acessar a mensagem com e.getMessage().
+    //HttpServletRequest request: Representa a requisição HTTP que permite objet dados como URI, método HTTP e Headers.
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        // Descrição do erro.
+        String error = "Database error!";
+        // O status HTTP 400.
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        // Montado o objeto que será convertido em JSON, que retornará os seguintes dados:
+        StandardError err = new StandardError(
+                // Data/hora atual
+                Instant.now(),
+                // 400
+                status.value(),
+                // "Resource not found!"
+                error,
+                // Mensagem da exceção
+                e.getMessage(),
+                // URI da requisição
+                request.getRequestURI());
+        // Constrói a resposta HTTP com status 400 e o Body retorna o objeto StandardError que é convertido para JSON automáticamente pelo Jackson
         return ResponseEntity.status(status).body(err);
     }
 
